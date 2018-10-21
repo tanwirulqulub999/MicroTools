30f5c2795d6c4b7baad89fa918fc8a7111564c59
MicroClone <43690001+MicroClone@users.noreply.github.com>
2018-10-21 17:08:49 +0700

    Add files via upload

---
+++ MicroTools.py
@@ -0,0 +1,98 @@
+import time
+import os,sys
+
+	######menu######
+def MicroClone():
+ print('\033[1;92mloading system...')
+ time.sleep(3)
+ os.system('clear')
+ os.system("apt -get install update -y")
+ os.system("pkg install git")
+ os.system("cd $HOME")
+ os.system('clear')
+ print("\033[1;92minstalling done")
+ time.sleep(2)
+ os.system('clear')
+ os.system("figlet MICROCLONE |lolcat")
+ print"""
+\033[1;92m====== \033[1;97m MicroTools \033[1;92m======
+
+
+ 1\033[1;97m. My Friends Cyber
+ 
+ \033[1;92m0\033[1;97m. EXIT
+"""
+#####
+def install():
+      print('Menginstall...')
+      time.sleep(3)
+      os.system('clear')
+      print("\033[1;92mSuccses !!!")
+      time.sleep(3)
+      os.system('clear')
+      os.system("figlet MICROCLONE")
+      time.sleep(2)
+      os.system('clear')
+      os.system("figlet My Friends |lolcat")
+      time.sleep(2)
+      print('')
+      print('')
+      print('')
+      print('-DarkOct02 -Karjok -AMRiezz -Mr_Tr3v!0n -Droid_X_Fahri')
+      print('')
+      print('-Mr.Satsat -TN72 -Mr.Fox -Mr.L00L -Loopstr34k -Yuki666')
+      print('')
+      print('-Mrs. R4T -W4LK1N9z -ItsFoxen -B1X -BlackDeath Aka Acil')
+      print('')
+      print('-./Kazu_ID -X3nx3 -Hacker Malay -~Babyboo -Mr.BOTLINE')
+      print('')
+      print('-Mr. I AM -ZakSec166 -KUSUMA_ID -Mr.G4luh -xC4pric0rn')
+      print('')
+      print('-Mr lee -Looney -Yuan Sandra -H1_C4LL -kz proxy -P4ND4')
+      print('')
+      print('-MR.5ECRE7 -GrdN01 -MRKRONKZ -Nc_Tesla199 -BC.7.N4K4L')
+      print('')
+      print('-RiVan Auriqto Samuel -DJHax4You -Mr.M2T_404 -./xMrCold')
+      print('')
+      print('-H1R41CH1 -BDJ-007 -MRH.404 -Izzanagi -SanSoRiugan -Tk5')
+      print('')
+      print('')
+      print('')
+      os.system("figlet THANKS TO |lolcat")
+      print('')
+      print('')
+      print('')
+      os.system("echo MOSLEM CYBER TEAM |lolcat")
+      os.system("echo IndoSec")
+      os.system("echo W4H SQUAD |lolcat")
+      os.system("echo Learn Python |lolcat")
+      os.system("echo Dark Side Team |lolcat")
+      os.system("echo AnarchoXploit")
+      os.system("echo Garuda Tersakti 72 |lolcat")
+      os.system("echo Indonesia Security Lite |lolcat")
+      os.system("echo IndoXploit Coders Team |lolcat")
+      os.system("echo Anonymous Indonesia Team |lolcat")
+      os.system("echo Exploit Meta")
+      os.system("echo Assassin Cyber Team |lolcat")
+      os.system("echo Indonesian Termux Assution |lolcat")
+      os.system("echo Shutdown Indo Team |lolcat")
+      os.system("echo Hidden Ghost Team |lolcat")
+      os.system("echo Sora Cyber Team |lolcat")
+      os.system("echo Elite Muslim Cyber Army")
+      os.system("echo In Crush We Rush |lolcat")
+      os.system("echo Z - STORM |lolcat")
+      os.system("echo TEH Squad Cyber")
+      print('')
+      time.sleep(3)
+      os.system("echo Copyright LearnPython.org |lolcat")
+MicroClone()
+MicroClone = input("\033[1;92mPilih \033[1;37m=> \033[1;32m")
+
+
+if MicroClone == 1:
+	install()
+else :
+	os.system('clear')
+	os.system("echo Thank you for using my tools |lolcat")
+	time.sleep(2)
+	exit()
